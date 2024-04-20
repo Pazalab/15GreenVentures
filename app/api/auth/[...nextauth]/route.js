aeaad5457dc00df9369ba915c7e5d6d03bf079cf
justallyt <albertjunk2030@gmail.com>
2024-04-20 23:21:20 +0300

    Updated env

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,8 +9,8 @@ import clientPromise from "@/lib/db";
 export const authOptions = {
         providers: [
                GoogleProvider({
-                       clientId: process.env.GOOGLE_CLIENT_ID,
-                       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+                       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ,
+                       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
                        allowDangerousEmailAccountLinking: true,
                        profile(profile){
                             return {
